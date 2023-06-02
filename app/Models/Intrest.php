73931ba8346c13ced23ef6a279da570397581784
Ammarvtech <ammar.vtechnocrat@gmail.com>
2023-06-02 00:52:44 -0700

    update

--- app/Models/Intrest.php
+++ app/Models/Intrest.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Intrest extends Model
 {
     use HasFactory;
-    protected $table = 'Intrests';
+    protected $table = 'intrests';
 
     public function booking()
     {
