1a33fae70695977c1b0e9129a7fc1bc6ea02d5f1
ammardevdksa <ammar.vtechnocrat@gmail.com>
2023-06-22 15:30:10 +0500

    update

--- database/factories/NumberOfTeamFactory.php
+++ database/factories/NumberOfTeamFactory.php
@@ -12,12 +12,12 @@ class NumberOfTeamFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array<string, mixed> 
      */
     public function definition(): array
     {
         return [
-            'number_of_teams' => fake()->unique()->word,
+            'number_of_teams' => $this->faker->numberBetween(1, 10),
             'created_by' => null,
             'updated_by' => null,
             'deleted_by' => null,
